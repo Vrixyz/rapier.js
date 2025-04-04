5b7098f6c54c3f0d49030f8fbb416204100f8f98
Thierry Berger <contact@thierryberger.com>
2025-04-04 12:54:19 +0200

    removed serialization + debug render + control ; seems not enough :thinking:

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,13 +1,13 @@
-pub use self::debug_render_pipeline::*;
+//pub use self::debug_render_pipeline::*;
 pub use self::event_queue::*;
 pub use self::physics_hooks::*;
 pub use self::physics_pipeline::*;
 pub use self::query_pipeline::*;
-pub use self::serialization_pipeline::*;
+//pub use self::serialization_pipeline::*;
 
-mod debug_render_pipeline;
+//mod debug_render_pipeline;
 mod event_queue;
 mod physics_hooks;
 mod physics_pipeline;
 mod query_pipeline;
-mod serialization_pipeline;
+// mod serialization_pipeline;
